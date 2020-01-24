294158695cc5c5a419e6dd621e56929fdd30f905
SilasCerqueira <silasfereira@gmail.com>
2020-01-24 18:05:38 -0300

    commit initial

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/tabs/products_tab.dart';
+import '../tabs/home_tab.dart';
+import '../widgets/custom_drawer.dart';
+
+class HomeScrean extends StatelessWidget {
+
+  final _pageController = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+       Scaffold(
+         body: HomeTab(),
+         drawer: CustomDrawer(_pageController),
+       ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
+        Container(color: Colors.black,),
+        Container(color: Colors.white,),
+      ],
+    );
+  }
+}
